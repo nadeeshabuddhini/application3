788fe34a20b270ad659157fdc84e962d5918c0bd
Nadeesha Buddhini <hknadeesha1995buddhi@gmail.com>
2019-02-21 22:07:08 +0530

    application3
    application3

---
+++ src/application/pkg3/Application3.java
@@ -0,0 +1,13 @@
+
+package application.pkg3;
+public class Application3 {
+
+     
+    public static void main(String[] args) {
+     for(int x=0; x<10; x++)
+     {
+         System.out.println(x);
+     }
+    }
+    
+}
